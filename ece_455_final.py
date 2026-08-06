ef91eaad29a7a006d48f012714e063b7a5dd1041
Antarpreet Singh <as2khals@uwaterloo.ca>
2026-08-06 01:12:14 -0400

    feat: scale tasks to integer time and compute hyperperiod

--- ece_455_final.py
+++ ece_455_final.py
@@ -1,4 +1,6 @@
 import sys
+from math import gcd
+from functools import reduce
 
 
 def parse_tasks(filename):
@@ -12,11 +14,28 @@ def parse_tasks(filename):
     return tasks
 
 
+SCALE = 1000
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+def scale_tasks(tasks):
+    return [(round(e * SCALE), round(p * SCALE), round(d * SCALE)) for e, p, d in tasks]
+
+
+def compute_hyperperiod(tasks_int):
+    return reduce(lcm, [p for _, p, _ in tasks_int])
+
+
 def main():
     if len(sys.argv) != 2:
         sys.exit(1)
     filename = sys.argv[1]
     tasks = parse_tasks(filename)
+    tasks_int = scale_tasks(tasks)
+    hyperperiod = compute_hyperperiod(tasks_int)
 
 
 if __name__ == "__main__":
