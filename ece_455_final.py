16b14107597be6b2fe2479074e0b6c6cb7e613da
Antarpreet Singh <as2khals@uwaterloo.ca>
2026-08-06 01:11:18 -0400

    feat: parse task input from file

--- ece_455_final.py
+++ ece_455_final.py
@@ -1,11 +1,24 @@
 import sys
 
 
+def parse_tasks(filename):
+    tasks = []
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                e, p, d = [float(x) for x in line.split(',')]
+                tasks.append((e, p, d))
+    return tasks
+
+
 def main():
     if len(sys.argv) != 2:
         sys.exit(1)
     filename = sys.argv[1]
+    tasks = parse_tasks(filename)
 
 
 if __name__ == "__main__":
     main()
+
